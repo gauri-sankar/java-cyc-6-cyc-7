2e4511e744539d4c59146fc25590510dd84e9731
gauri-sankar <96428549+gauri-sankar@users.noreply.github.com>
2022-02-15 12:49:55 +0530

    Create experiment1a.java

---
+++ experiment1a.java
@@ -0,0 +1,17 @@
+ 
+ ​class​ ​experiment1a 
+ ​{ 
+ ​        ​public​ ​static​ ​void​ ​main​(​String​[] ​args​) 
+ ​        { 
+ ​                ​try 
+ ​                        { 
+ ​                ​int​ b, a ​=​ ​5​; 
+ ​                b ​=​ a​/​0​; 
+ ​                ​System​.​out​.​println(b); 
+ ​                        } 
+ ​                ​catch​(​ArithmeticException​ e) 
+ ​                        { 
+ ​                                ​System​.​out​.​println(e); 
+ ​                        } 
+ ​        }         
+ ​}        
